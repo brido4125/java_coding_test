5d32b526e038c464cf8479910420d0ea7bca421e
hcs4125 <hcs4125@gmail.com>
2021-09-24 20:26:08 +0900

    ReverseChar

---
+++ src/string/ReverseChar.java
@@ -0,0 +1,29 @@
+package string;
+
+import java.util.Scanner;
+
+public class ReverseChar {
+    public static String solution(String str) {
+        String answer = "";
+        char[] chars = str.toCharArray();
+        int lt = 0, rt = chars.length - 1;
+        while (lt < rt) {
+            // if both indexes are Alphabet?
+            if(Character.isAlphabetic(chars[lt]) && Character.isAlphabetic(chars[rt])){
+                char tmp = chars[lt];
+                chars[lt] = chars[rt];
+                chars[rt] = tmp;
+            }
+            lt++;
+            rt--;
+        }
+        answer = String.valueOf(chars);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println("solution(str) = " + solution(str));
+    }
+}
