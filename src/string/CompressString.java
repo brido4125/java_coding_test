a489cebb1b13f0949108941870eb92b5d4f89da9
hcs4125 <hcs4125@gmail.com>
2021-10-01 17:57:59 +0900

    문자열 압축

---
+++ src/string/CompressString.java
@@ -0,0 +1,33 @@
+package string;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class CompressString {
+    public static String solution(String str) {
+        StringBuilder answer = new StringBuilder();
+        str += " ";
+        char[] chars = str.toCharArray();
+        int cnt = 1;
+        for (int i = 0; i < chars.length-1; i++) {
+            if (chars[i] == chars[i + 1]) {
+                cnt++;
+            }else{
+                answer.append(chars[i]);
+                if(cnt != 1){
+                    answer.append(cnt);
+                }
+                cnt = 1;
+            }
+        }
+        return answer.toString();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String next = sc.next();
+        System.out.println("solution(next) = " + solution(next));
+
+    }
+}
